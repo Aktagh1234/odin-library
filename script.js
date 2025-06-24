0e8df159e81c6410ed84cbb7171aae4e3c006d54
Aashi <165303579+Aktagh1234@users.noreply.github.com>
2025-06-24 07:35:47 +0000

    Basic Library App Created

---
+++ script.js
@@ -0,0 +1,67 @@
+let myLibrary = [];
+
+function Book(title, author, pages, read){
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+}
+
+Book.prototype.toggleRead = function() {
+    this.read = !this.read;
+}
+
+function toggleRead(index){
+    myLibrary[index].toggleRead();
+    render();
+}
+
+function render(){
+    let libraryEl = document.querySelector("#book-list");
+    libraryEl.innerHTML = ""; 
+    for (let i = 0; i < myLibrary.length; i++) {
+        let book = myLibrary[i];
+        let bookEl = document.createElement("div");
+        bookEl.setAttribute("class", "book-card");
+        bookEl.innerHTML = `
+        <div class="card-header">
+        <h3 class="title">${book.title}</h3>
+        <h5 class="author">${book.author}</h5>
+        </div> 
+        <div class="card-body">
+        <p>${book.pages} pages</p>
+        <p class="read-status">${book.read ? "Read" : "Not Read"}</p>
+        <button class="remove-btn" onclick="removeBook(${i})">Remove</button>
+        </div>
+        `;
+        libraryEl.appendChild(bookEl);
+    }
+}
+
+function removeBook(index) {
+    myLibrary.splice(index, 1);
+    render();
+}
+
+function addBookToLibrary() {
+    let title = document.querySelector("#title").value;
+    let author = document.querySelector("#author").value;
+    let pages = document.querySelector("#pages").value;
+    let read = document.querySelector("#read").checked;
+    let newBook = new Book(title, author, pages, read);
+    myLibrary.push(newBook);
+    render();
+}
+
+let newBookButton = document.querySelector("#new-book-btn");
+let newBookForm = document.querySelector("#new-book-from");
+
+newBookButton.addEventListener("click", function() {
+    newBookForm.style.display = "flex";
+});
+
+document.querySelector("#new-book-from").addEventListener("submit", function(event) {
+    event.preventDefault();
+    addBookToLibrary();
+    newBookForm.reset();
+});
